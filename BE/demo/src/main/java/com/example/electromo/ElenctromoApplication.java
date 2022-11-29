ab61f884675ded95ddf9750719f53243da63932d
Abilash Shankarpalli <abilashece.rymec@gmail.com>
2022-11-29 23:17:16 +0530

    started code for both FE and BE

---
+++ BE/demo/src/main/java/com/example/electromo/ElenctromoApplication.java
@@ -0,0 +1,12 @@
+package com.example.electromo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElenctromoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElenctromoApplication.class, args);
+	}
+}
---
+++ BE/demo/src/test/java/com/example/electromo/ElenctromoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.electromo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElenctromoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
